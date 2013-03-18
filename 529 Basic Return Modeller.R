e33ddb6d7b480889abef1b66b0297641a471e31d
Rohit Sharma <rohit@utexas.edu>
2013-03-17 21:24:04 -0700

    Inital Code
    Models Returns for 529 portfolio

---
+++ 529 Basic Return Modeller.R
@@ -0,0 +1,62 @@
+library("quantmod")  #Load the quantmod Library
+investAmntMonthly <-1000 #How much we want to invest each month
+stockData <- new.env() #Make a new environment for quantmod to store data in
+#DivData <- new.env()
+tickers <- c("VITPX","VIDMX","VBTLX") #Define the tickers we are interested in
+
+#Model
+allocation<-c(.8,0.2,0.2)
+
+startDate<-as.Date("2002-01-01") #Specify period of time we are interested in
+endDate<-as.Date("2012-12-31")
+
+#Download the stock history (for all tickers)
+getSymbols(tickers, env = stockData, src = "yahoo",from = startDate, to = endDate)
+#getDividends(tickers, env = DivData, src = "yahoo",from = startDate, to = endDate)
+cpi<-read.csv('http://www.quandl.com/api/v1/datasets/IMF/PCPI_111.csv?&auth_token=1D5mLmJ5LGDV8yPyp1zA&trim_start=2001-01-01&trim_end=2012-12-31&collapse=annual&transformation=rdiff&sort_order=asc', colClasses=c('Year'='Date'))
+cpits <- xts(cpi[,-1], order.by=cpi[,1])
+avgcpi<-mean(cpits)
+
+TotalSharesMonthly<-list()
+TotalSharesYearly<-list()
+TotalSharesYearlyR<-list()
+TotalValueMonthly<-list()
+TotalValueYearly<-list()
+TotalValueYearlyR<-list()
+
+for(i in 1:length(tickers)) 
+{
+  lastCloseM<-(Cl(to.period((get(tickers[i],stockData)),'months')))
+  AdCloseM<-(Ad(to.period((get(tickers[i],stockData)),'months')))
+  lastCloseY<-(Cl(to.period((get(tickers[i],stockData)),'years')))
+  AdCloseY<-(Ad(to.period((get(tickers[i],stockData)),'years')))
+      
+  MonthlyShares<-investAmntMonthly*allocation[i]/lastCloseM
+  YearlyShares<-investAmntMonthly*12*allocation[i]/lastCloseY
+  
+  TotalSharesMonthly[i]<-c(as.numeric(last(cumsum(MonthlyShares))))
+  TotalSharesYearly[i]<-c(as.numeric(last(cumsum(YearlyShares))))
+      
+  TotalValueMonthly[i]<-c((as.numeric(TotalSharesMonthly[i])*as.numeric(last(AdCloseM))))
+  TotalValueYearly[i]<-c((as.numeric(TotalSharesYearly[i])*as.numeric(last(AdCloseY))))      
+      
+}
+
+FinalMonthly<-sum(unlist(TotalValueMonthly))
+FinalYearly<-sum(unlist(TotalValueYearly))
+
+FinalYearlR<-sum(unlist(TotalValueYearlyR))
+
+TotalYears<-round(as.numeric((endDate-startDate)/365))
+TotalInvestment<-TotalYears*investAmntMonthly*12
+TotalInvestmentR<-investAmntMonthly*12*(((1+avgcpi)^TotalYears-1)/avgcpi)
+
+invretM<-100*(FinalMonthly/TotalInvestment-1)
+invretY<-100*(FinalYearly/TotalInvestment-1)
+invertR<-100*(FinalYearly/TotalInvestmentR-1)
+
+FinalMonthly
+FinalYearly
+invretM
+invretY
+invertR
